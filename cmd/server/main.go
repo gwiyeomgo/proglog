e30fc4cde93b87e50c94ba9690b9579453348096
gwiyeomgo <gwiyeomgo@gmail.com>
2023-05-24 23:57:53 +0900

    20230524 커밋 로그 서비스 실습

---
+++ cmd/server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gwiyeomgo/prolog/internal/server"
+	"log"
+)
+
+func main() {
+	srv := server.NewHTTPServer(":8080")
+	log.Fatal(srv.ListenAndServe())
+}
+
+/*
+
+curl -X POST localhost:8080 -d \ '{"record":{"value":"REWRWERWEFSD"}}'
+curl -X GET localhost:8080 -d \ '{"offset":0}'
+*/
